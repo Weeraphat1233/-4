d3fb16302cecadb030fa8f05a248465621dff49c
Weeraphat1233 <huntasirilas@gmail.com>
2025-07-20 00:02:06 +0700

    โปรแกรมตรวจสอบ Username และ Password.py

---
+++ โปรแกรมตรวจสอบ Username และ Password.py
@@ -0,0 +1,7 @@
+username = input("กรอก Username: ")
+password = input("กรอก Password: ")
+
+if username == "admin" and password == "Ad13n@23t":
+    print("ยินดีต้อนรับ Admin")
+else:
+    print("ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง")
