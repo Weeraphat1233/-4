7911be663c134e59ce0ddece726be910cbd6851e
Weeraphat1233 <huntasirilas@gmail.com>
2025-07-20 00:05:07 +0700

    BMI.py

---
+++ BMI.py
@@ -0,0 +1,13 @@
+weight = float(input("Weight (kg): "))
+height = float(input("Height (m): "))
+bmi = weight / (height ** 2)
+print(f"BMI is {bmi:.1f}")
+
+if bmi < 18.5:
+    print("Underweight")
+elif bmi < 25:
+    print("Normal weight")
+elif bmi < 30:
+    print("Overweight")
+else:
+    print("Obesity")
